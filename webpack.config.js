c3d9950f9a638492472aae63610332c6f358cf41
SupremeERG <ethang717163@icloud.com>
2023-05-06 10:27:16 -0500

    removed everything

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path")
-    // make sure these are the right contents to output
-module.exports = {
-  entry: './scripts/background.js',
-  output: {
-    filename: 'background.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
